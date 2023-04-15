cb20ebd1d019639d10bf48599b9941ee3adbc588
1mzunye-777 <geoffreymuli2000@gmail.com>
2023-04-15 09:02:58 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * _strdup - duplicate to new memory space location :)
+ * @str: char
+ * Return: 0
  */
-int main(void)
-{
-    char *s;
 
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+char *_strdup(char *str)
+{
+	char *duplc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	duplc = malloc(strlen(str) + 1);
+	if (duplc != NULL)
+	{
+		strcpy(duplc, str);
+	}
+	return (duplc);
+
 }
