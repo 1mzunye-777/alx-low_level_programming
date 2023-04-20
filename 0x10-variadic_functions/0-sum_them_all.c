2c6ee7153bde56be8b1746e316b1010498c2dd83
1mzunye-777 <geoffreymuli2000@gmail.com>
2023-04-20 08:02:55 -0700

    eauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,22 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
- */
+*sum_them_all - function that calculate the sum of all it parameter
+*@n: argument pass to the function
+*Return: the result of sum
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-	va_start(args, n);
+	va_start(list, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-	va_end(args);
-
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
 	return (sum);
 }
